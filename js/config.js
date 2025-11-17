7832248552893c362cf7a47da7eda15d9d8edb37
Anurag <anuragbaiju03@gmail.com>
2025-11-17 20:31:47 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_bUouTJkkt', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5uscjvr3kb54jbol0865fk7rdq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_Mx51qZnzr', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '44dogool6i8cg0l1jbrhfr0arq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
